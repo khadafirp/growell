f14ba43dd9f4fc1a2155ea941025283ac4aafbf9
Khadafi Rohman Prihanda <dafi.pri19@gmail.com>
2022-05-10 19:18:08 +0700

    add camera and file

--- lib/presentation/landing_page/penjual/produk/add_produk_page.dart
+++ lib/presentation/landing_page/penjual/produk/add_produk_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:growell/widget/child_page/custom_child_page.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddProdukPage extends StatefulWidget {
   AddProdukPage({Key? key}) : super(key: key);
@@ -9,6 +10,59 @@ class AddProdukPage extends StatefulWidget {
 }
 
 class _AddProdukPageState extends State<AddProdukPage> {
+  ImagePicker picker = ImagePicker();
+
+  openCamera() async {
+    final XFile? image = await picker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 200,
+      maxHeight: 200,
+      imageQuality: 70,
+    );
+    print(image.toString());
+  }
+
+  openGalery() async {
+    final XFile? image = await picker.pickImage(
+      source: ImageSource.gallery,
+      maxWidth: 200,
+      maxHeight: 200,
+      imageQuality: 70,
+    );
+    print(image.toString());
+  }
+
+  void _showPicker(context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext bc) {
+        return SafeArea(
+          child: Container(
+            child: new Wrap(
+              children: <Widget>[
+                new ListTile(
+                    leading: new Icon(Icons.photo_library),
+                    title: new Text('Photo Library'),
+                    onTap: () {
+                      openGalery();
+                      Navigator.of(context).pop();
+                    }),
+                new ListTile(
+                  leading: new Icon(Icons.photo_camera),
+                  title: new Text('Camera'),
+                  onTap: () {
+                    openCamera();
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+          ),
+        );
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return CustomChildPage(
@@ -117,15 +171,18 @@ class _AddProdukPageState extends State<AddProdukPage> {
                   width: MediaQuery.of(context).size.width * 0.80,
                   child: Row(
                     children: [
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.20,
-                        height: MediaQuery.of(context).size.height * 0.04,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          color: Colors.grey[200],
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                      InkWell(
+                        onTap: () => _showPicker(context),
+                        child: Container(
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.all(16),
+                          margin: EdgeInsets.only(top: 16),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                          child: Text("Pilih Foto"),
                         ),
-                        child: Text("Pilih Foto"),
                       )
                     ],
                   ),
